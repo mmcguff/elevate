5f1f132a856b90b4363ca9d40d6a7dc860c6430e
Matthew McGuff <matthew.mcguff@walmart.com>
2019-09-30 21:37:01 -0500

    add building class

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 //Entry point to my application
+const Building = require('./building');
+
+function start(){
+    console.log('Welcome to Elevate! An app that simulates movements of an elevator.');
+    console.log('==================================================================');
+}
+
+
+start();
--- building.js
+++ building.js
@@ -1,1 +1,31 @@
 //building class
+const Elevator = require('./elevator');
+
+class Building {
+
+    constructor(name, floors){
+        this.name = name; //name of the building
+        this.floors = floors; //number of floors in the building
+        this.elevators = []; //array of elevators (will need to import this)
+    }
+
+    //Getter retruning inof about an initilized building
+    getBuildingInfo(){
+        return {
+            buildingName: this.name,
+            floors: this.floors,
+            elevators: this.elevators
+        }; 
+    }
+
+    //Adding an elevator to the building by name
+    setElevator(elevatorName){
+        this.elevators.push(new Elevator());
+    }
+
+    //The request of elevator including the floor the request was made and up or down
+    pressButtonForElevator(floor, direction){
+    }
+}
+
+module.exports = Building;
