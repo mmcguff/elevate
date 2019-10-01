5feadb5455728abdbb63fc4dc6f09fbbf20fcae3
Matthew McGuff <matthew.mcguff@walmart.com>
2019-09-30 21:52:57 -0500

    Building out Elevator Class

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 //Entry point to my application
 const Building = require('./building');
 
+//create a new building instance
+const bldg = new Building('Kuali Building', 10);
+
+//create a new instance for each elevator 
+
+
 function start(){
     console.log('Welcome to Elevate! An app that simulates movements of an elevator.');
     console.log('==================================================================');
+    console.log(bldg.getBuildingInfo());
 }
 
 
--- elevator.js
+++ elevator.js
@@ -1,1 +1,19 @@
 //elevator class
+
+class Elevator {
+    
+    constructor(name, currentFloor){
+        this.name = name;
+        this.maxTrips = 100;  //configurable but hardcoded per the spec
+
+        this.isRunning = true;  
+        this.currentFloor = currentFloor; //User can decide where elevator starts.
+        this.direction = 'up' //up down or none
+        this.isDoorClosed = true // false if door is open. only two states
+        this.travelQueue = [];  //array contains values of floors traveling to.
+        this.tripCount = 0; //number of complete trips
+        this.floorsTravledCount = 0; //number of floors travled    
+    }
+
+    
+}
